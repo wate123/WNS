f88b313d73286fa5bcd24eb09d18968a8fe278da
Jun Lin <junlin0604@gmail.com>
2017-04-20 04:22:05 -0500

    New UI and using Firebase to store user information

--- app/src/main/java/weathernotificationservice/wns/Download.java
+++ app/src/main/java/weathernotificationservice/wns/Download.java
@@ -1,63 +1,63 @@
-package weathernotificationservice.wns;
-
-import android.os.AsyncTask;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-/**
- * Created by Jun Lin on 4/19/2017.
- */
-
-public class Download extends AsyncTask<String,Void, String> {
-
-    @Override
-    protected String doInBackground(String... urls) {
-        String result = "";
-        URL url;
-        HttpURLConnection urlConnection = null;
-        try {
-            url = new URL(urls[0]);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = urlConnection.getInputStream();
-            InputStreamReader reader = new InputStreamReader(in);
-            int data = reader.read();
-
-            while (data != -1){
-                char current = (char) data;
-                result += current;
-                data = reader.read();
-            }
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    protected void onPostExcute(String result){
-        super.onPostExecute(result);
-        try {
-            JSONObject jsonObj = new JSONObject(result);
-
-            JSONObject weatherData = new JSONObject(jsonObj.getString("main"));
-            double tempInt = Double.parseDouble(weatherData.getString("temp"));
-            //convert C to F
-            int tempIn = (int) (tempInt*1.8-459.67);
-            //get the name from json
-            String placeName = jsonObj.getString("name");
-            MainActivity.tempText.setText(String.valueOf(tempIn));
-            MainActivity.placeText.setText(placeName);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package weathernotificationservice.wns;
+//
+//import android.os.AsyncTask;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//
+///**
+// * Created by Jun Lin on 4/19/2017.
+// */
+//
+//public class Download extends AsyncTask<String,Void, String> {
+//
+//    @Override
+//    protected String doInBackground(String... urls) {
+//        String result = "";
+//        URL url;
+//        HttpURLConnection urlConnection = null;
+//        try {
+//            url = new URL(urls[0]);
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//            InputStream in = urlConnection.getInputStream();
+//            InputStreamReader reader = new InputStreamReader(in);
+//            int data = reader.read();
+//
+//            while (data != -1){
+//                char current = (char) data;
+//                result += current;
+//                data = reader.read();
+//            }
+//            return result;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    protected void onPostExcute(String result){
+//        super.onPostExecute(result);
+//        try {
+//            JSONObject jsonObj = new JSONObject(result);
+//
+//            JSONObject weatherData = new JSONObject(jsonObj.getString("main"));
+//            double tempInt = Double.parseDouble(weatherData.getString("temp"));
+//            //convert C to F
+//            int tempIn = (int) (tempInt*1.8-459.67);
+//            //get the name from json
+//            String placeName = jsonObj.getString("name");
+//            MainActivity.tempText.setText(String.valueOf(tempIn));
+//            MainActivity.placeText.setText(placeName);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- app/src/main/java/weathernotificationservice/wns/LoginActivity.java
+++ app/src/main/java/weathernotificationservice/wns/LoginActivity.java
@@ -1,209 +1,114 @@
 package weathernotificationservice.wns;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.Profile;
-import com.facebook.ProfileTracker;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String HOME_ACTIVITIES = "weathernotificationservice.wns.activity_main";
-    private static final String TAG = "LoginActivity";
-    private static final int REQUEST_SIGNUP = 0;
 
-    @Bind(R.id.input_email) EditText _emailText;
-    @Bind(R.id.input_password) EditText _passwordText;
-    @Bind(R.id.btn_login) Button _loginButton;
-    @Bind(R.id.link_signup) TextView _signupLink;
-    private LoginButton loginButton;
-    private CallbackManager callbackManager;
-    private AccessTokenTracker accessTokenTracker;
-    private AccessToken accessToken;
-    private ProfileTracker profileTracker;
-
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private Button btnSignup, btnLogin, btnReset;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
+        // set the view now
         setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
 
-        _loginButton.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
             }
         });
 
-        _signupLink.setOnClickListener(new View.OnClickListener() {
-
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start the Signup activity
-                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivityForResult(intent, REQUEST_SIGNUP);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
             }
         });
 
-        //Facebook
-        callbackManager= CallbackManager.Factory.create();
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email", "public_profile");
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d("LOGIN_SUCCESS", "Success");
-                Toast.makeText(getBaseContext(), "Login Success", Toast.LENGTH_LONG).show();
-                finish();//<- IMPORTANT
-            }
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
 
-            @Override
-            public void onCancel() {
-                Log.d("LOGIN_CANCEL", "Cancel");
-            }
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-            @Override
-            public void onError(FacebookException error) {
-                Log.d("LOGIN_ERROR", "Error");
-                Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
-
-
-        accessTokenTracker = new AccessTokenTracker() {
-            @Override
-            protected void onCurrentAccessTokenChanged(
-                    AccessToken oldAccessToken,
-                    AccessToken currentAccessToken) {
-                        // Set the access token using
-                        // currentAccessToken when it's loaded or set.
-                    }
-                };
-        // If the access token is available already assign it.
-        accessToken = AccessToken.getCurrentAccessToken();
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(
-                    Profile oldProfile,
-                    Profile currentProfile) {
-                // App code
-            }
-        };
-    }
-
-
-    public void login() {
-        Log.d(TAG, "Login");
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGNUP) {
-            if (resultCode == RESULT_OK) {
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                this.finish();
-            }
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        this.finish();
-    }
-
-    @Override
-    public void onBackPressed() {
-        // Disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
-
-    public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
-        finish();
-    }
-
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _loginButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        accessTokenTracker.stopTracking();
     }
 }
+
